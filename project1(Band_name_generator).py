18c0f998357c326e30c0d0b05be04cf100aaa2db
Ronin2494 <deepaksharma2494@gmail.com>
2023-10-11 17:28:58 -0400

    Project1_file

---
+++ project1(Band_name_generator).py
@@ -0,0 +1,7 @@
+print("Hello, Welcome to the program!")
+
+city = input("Please enter the city you grew up in:\n")
+
+pet_name = input("Please enter the name of a pet:\n")
+
+print("So, your band name is:\n" + city + " " + pet_name)
