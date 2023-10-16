b36a1de6c3f5ea46f38ea017bc889bf28aa4b082
Ronin2494 <deepaksharma2494@gmail.com>
2023-10-16 17:42:23 -0400

    maze project

--- Projects/Maze.py
+++ Projects/Maze.py
@@ -1,27 +1,21 @@
-######Reeborg hurdle 4#########
 def turn_right():
     turn_left()
     turn_left()
     turn_left()
     
-def jump(): 
-    turn_left()
-    while wall_on_right():
-        move()
-    turn_right()
+while front_is_clear():
     move()
-    turn_right()
-    move()
-    while front_is_clear():
-        move()
-    turn_left()
+turn_left()
+    
             
 while at_goal()!= True:
-    if wall_in_front():
-        jump()
-    
-    else:
+    if right_is_clear():
+        turn_right()
         move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
 ################################################################
 # WARNING: Do not change this comment.
 # Library Code is below.
